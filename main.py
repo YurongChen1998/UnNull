0bea41c3b377a407a942d8126a248e34bb9416f2
Yurong <43307048+YurongChen1998@users.noreply.github.com>
2025-01-01 21:27:07 +0800

    1

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     
     
     if MSFA:
-        # 'CAVE_balloons' (3), 'CAVE_beads' (0.5), 'CAVE_cd' (0.1), 'CAVE_chart' (1), 'CAVE_clay' (1), 'CAVE_cloth' (5), 'CAVE_fake_bear' (1), 'CAVE_feathers' (1), 'CAVE_flowers' (3), 'CAVE_oil'
+        # 'CAVE_balloons' (3), 'CAVE_beads' (0.5), 'CAVE_cd' (0.1), 'CAVE_chart' (1), 'CAVE_clay' (1), 'CAVE_cloth' (5), 'CAVE_fake_bear' (1), 'CAVE_feathers' (1), 'CAVE_flowers' (3), 'CAVE_oil' （7）
         data_list = ['CAVE_oil'] 
         for file_name in data_list:
             main_MSFA_CAVE(file_name)
